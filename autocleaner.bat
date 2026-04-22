8c0f00e74434fc1d577d8850db53d7fb55072032
DumbOut <johnmuja76@gmail.com>
2026-04-22 11:21:13 -0500

    Update autocleaner.bat

--- autocleaner.bat
+++ autocleaner.bat
@@ -1,6 +1,7 @@
 @echo off
 rem Ready to go
 del /F data\world\maps\campaign\imperial_campaign\map.rwm
+del /F data\world\maps\base\map.rwm
 echo.
 echo CLEANED. ERROR MESSAGES ARE NORMAL HERE
 rem pause
